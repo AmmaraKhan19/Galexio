50a8719cae4fcca71f082a3d13f2d930e75a4f62
Ammara Khan <ammarak281@gmail.com>
2023-03-09 21:57:08 +0500

    news app categorized

--- src/App.js
+++ src/App.js
@@ -4,13 +4,25 @@ import './App.css';
 import React, { Component } from 'react'
 import Navbar from './components/Navbar';
 import News from './components/News';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <Navbar />
-        <News />
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route element={<News pagesize = {6} key='general' country = "us" category="general" />} exact path='/' />
+            <Route element={<News pagesize = {6} key='business' country = "us" category="business" />} exact path='/business' />
+            <Route element={<News pagesize = {6} key='science' country = "us" category="science" />} exact path='/science' />
+            <Route element={<News pagesize = {6} key='technology' country = "us" category="technology" />} exact path='/technology' />
+          </Routes>
+        </Router>
       </div>
     )
   }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
+
+  imageFail = (event) => {
+    event.target.src = "https://cdn.mos.cms.futurecdn.net/838Uzpa69pphA8Vzq7GJo9-1200-80.jpg"
+ }
   
   render() {
     let {title, description, imgurl, newsurl} = this.props;
     return (
       <div className='my-3'>
-        <div className="card" style={{width: "18rem"}}>
-          <img src={imgurl} className="card-img-top" alt="..." />
+        <div className="card" >
+          <img src={!imgurl?"https://cdn.mos.cms.futurecdn.net/838Uzpa69pphA8Vzq7GJo9-1200-80.jpg":imgurl} onError={this.imageFail} className="card-img-top" alt="..." />
           <div className="card-body">
-            <h5 className="card-title">{title}...</h5>
-            <p className="card-text">{description}...</p>
-            <a href={newsurl} target={"_blank"} rel="noreferrer" className="btn btn-sm btn-primary">Read More</a>
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <a rel="noreferrer" href={newsurl} target={"_blank"} className="btn btn-sm btn-dark">Read More</a>
           </div>
         </div>
       </div>
